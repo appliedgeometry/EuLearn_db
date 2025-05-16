69d05f19d1754d7ebfad2a8e53b93904a23decc2
Fritz-Rodrigo <rodrigofritz@gmail.com>
2025-05-15 18:56:39 -0600

    First files upload

--- Architectures/pointnet_models/pointnetpp.py
+++ Architectures/pointnet_models/pointnetpp.py
@@ -10,34 +10,34 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 class PointNetSetAbstraction(nn.Module):
     def __init__(self, in_channel=3, mlp=[64,128], group_all=False, npoint=256, radius=10, nsample=64):
         super(PointNetSetAbstraction, self).__init__()
-        self.npoint = npoint # Valor de puntos agrupados
-        self.radius = radius # Radio del muestreo
-        self.nsample = nsample # Tamaño de muestra
+        self.npoint = npoint # Number of grouped points
+        self.radius = radius # Sampling radius
+        self.nsample = nsample # Sample size
         self.mlp_convs = nn.ModuleList()
         self.mlp_bns = nn.ModuleList()
         self.in_channel = in_channel
-        last_channel = in_channel # Canales de salida
-        for out_channel in mlp: # Crea capas de convolución
-            self.mlp_convs.append(nn.Conv2d(last_channel, out_channel, 1)) # Capa convolución
-            self.mlp_bns.append(nn.BatchNorm2d(out_channel)) # normalización de batches
+        last_channel = in_channel # Output channels
+        for out_channel in mlp: # Creates convolutional layers
+            self.mlp_convs.append(nn.Conv2d(last_channel, out_channel, 1)) # Convolutional layer
+            self.mlp_bns.append(nn.BatchNorm2d(out_channel)) # Bacth normalization
             last_channel = out_channel
         self.group_all = group_all
 
     def forward(self, xyz, points):
-        # Revisa si es agrupamiento final o intermedio
+        # Checks if it's final or intermediate grouping
         if self.group_all:
             new_xyz, new_points = sample_and_group_all(xyz, points)
         else:
             new_xyz, new_points = sample_and_group(self.npoint, self.radius, self.nsample, xyz, points)
             new_points = new_points[:,:,:,:self.in_channel]
 
-        # Aplica convoluciones y normalización
+        # Applies convolutions and normalization
         new_points = new_points.permute(0, 3, 2, 1)
         for i, conv in enumerate(self.mlp_convs):
             bn = self.mlp_bns[i]
             new_points =  F.relu(bn(conv(new_points)))
 
-        # Regresa la permutación y obtiene agragación máx
+        # Returns permutation and obtains max aggregation
         new_points = new_points.permute(0, 3, 2, 1)
         new_points = torch.max(new_points, 2)[0]
 
@@ -58,12 +58,12 @@ class PointNetPlusPlus(nn.Module):
 
     def forward(self, xyz):
         B, _, _ = xyz.shape
-        # Capas PintNet++
+        # PointNet++ Layers
         l1_xyz, l1_points = self.sa1(xyz, None)
         l2_xyz, l2_points = self.sa2(l1_xyz, l1_points)
         l3_xyz, l3_points = self.sa3(l2_xyz, l2_points)
 
-        # Classificación por FFW
+        # FFW Classification
         x = l3_points.view(B, self.dim)
         h1 = self.drop1(self.ffw1(x))
         out = self.ffw2(h1)
